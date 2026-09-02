538760f54d8a19ac794cfb30e04610c35b437eb1
qidewei2004 <qidewei2004@gmail.com>
2026-09-02 22:52:23 +0800

    放宽 Release 构建的严格并发检查

--- script/build_release_assets.sh
+++ script/build_release_assets.sh
@@ -242,6 +242,7 @@ xcodebuild \
   SWIFT_COMPILATION_MODE=singlefile \
   SWIFT_ENABLE_BATCH_MODE=NO \
   SWIFT_OPTIMIZATION_LEVEL=-Onone \
+  SWIFT_STRICT_CONCURRENCY=targeted \
   build
 
 APP_PATH="$(find "$DERIVED_DATA/Build/Products/Release" -maxdepth 1 -name "Surge Relay.app" -type d -print -quit)"
